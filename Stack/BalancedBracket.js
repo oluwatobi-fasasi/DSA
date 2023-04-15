5d205ccf182e28e3ed4f6cddea9bb14c456a7505
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-04-15 13:44:41 -0400

    DSA

--- Stack/BalancedBracket.js
+++ Stack/BalancedBracket.js
@@ -1,30 +1,20 @@
-function balancedBracket(string) {
+const balancedBracket = (string) => {
     let stack = [];
     for (let char of string) {
-        if (char === '{' || char === '[' || char === '(') {
+        if (char === '(' || char === '{' || char === '[')
             stack.push(char);
-        }
-        else if (char === '}' || char === ']' || char === ')') {
-            if ((char === '}' && stack[stack.length - 1] === '{') ||
-                (char === ']' && stack[stack.length - 1] === '[') ||
-                (char === ')' && stack[stack.length - 1] === '(')) {
-                stack.pop();
-            } else {
-                return false
-            }
+        else if (char === ')' || char === '}' || char === ']') {
+            if ((char === ')' && stack[stack.length - 1] === '(') ||
+                (char === '}' && stack[stack.length - 1] === '{') ||
+                (char === ']' && stack[stack.length - 1] === '['))
+                stack.pop()
+            else return false
         }
 
     }
-    return stack.length === 0;
+    return stack.length === 0
 }
 
-console.log(balancedBracket('(hello)[world]'))
-// => true
-
-console.log(balancedBracket('([)]'))
-// => false
-
-console.log(balancedBracket('[({}{}{})([])]'))
-// => true
-
-
+console.log(balancedBracket('(hello)[world]'));
+console.log(balancedBracket('([)]'));
+console.log(balancedBracket('[({}{}{})([])]'));
